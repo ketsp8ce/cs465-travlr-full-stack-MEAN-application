e0c3616655e53d218a436fbaf9a2614cb6bab54b
ketsp8ce <hunter.marx@snhu.edu>
2025-10-08 16:19:04 -0500

    Added route to index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+// This is where we import the controllers we will route
 const TripsController = require("../controllers/trips");
 
-router.route("/trips").get(TripsController.tripslist);
+// define route for our trips endpoint
+router
+    .route('/trips')
+    .get(TripsController.tripslist); // GET Method routes tripList
+
+// GET Method routes tripFindByCode
+router
+    .route('/trips/:tripCode')
+    .get(TripsController.tripsFindByCode);
 
 module.exports = router;
+
