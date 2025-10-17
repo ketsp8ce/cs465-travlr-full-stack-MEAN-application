3a805640d8faf1a565d4103f435c734e020deffc
ketsp8ce <hunter.marx@snhu.edu>
2025-10-16 19:49:13 -0500

    Fixed minor syntax errors

---
+++ app_admin/src/app/trip-listing/trip-listing.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-listing',
+  imports: [],
+  templateUrl: './trip-listing.html',
+  styleUrl: './trip-listing.css'
+})
+export class TripListing {
+
+}
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,22 @@
-/*GET travel view*/
-const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Gateways'});
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+method: 'GET',
+headers: {
+        'Accept': 'application/json'
+    },
+};
+
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('app_api/data/trips.json', 'utf8'));
+
+const travel = async function (req, res, next) {
+    // console.log('TRAVEL: CONTROLLER BEGIN');
+    await fetch(tripsEndpoint, options)
+        .then(res => res.json())
+        .then(json => {
+            res.render('travel', { title: 'Travlr Gateways', trips: json, message});
+        })
+    catch((err) -> res.status(500).send(err.message));
 };
 
 module.exports = {
